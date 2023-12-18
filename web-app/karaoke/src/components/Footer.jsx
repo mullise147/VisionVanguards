ad1088b5b34e1ba6247df828f3b5813668eb3240
Athulya Ganesh <athulya.ganesh2001@gmail.com>
2023-12-18 22:35:39 +0530

    meet the team

--- web-app/karaoke/src/components/Footer.jsx
+++ web-app/karaoke/src/components/Footer.jsx
@@ -1,8 +1,18 @@
-function Footer() 
-{
-    return (
-        <>This is the footer. </>
-    )
+import React from 'react';
 
-}
-export default Footer; 
+const Footer = () => {
+  return (
+    <footer className="footer-container">
+      <div className="footer-content">
+        <p>Developed by: The Vision Vanguards</p>
+        <ul>
+          <li><a href="mailto:lnuas@mail.uc.edu">Athulya Ganesh</a></li>
+          <li><a href="mailto:kelly2r@mail.uc.edu">Rob Kelly</a></li>
+          <li><a href="mailto:mullise@mail.uc.edu">Stephanie Mullins</a></li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- web-app/karaoke/src/components/LandingPage.jsx
+++ web-app/karaoke/src/components/LandingPage.jsx
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
 import "../../src/index.css"; 
+import Footer from "../components/Footer"; 
+import About from "../components/About"; 
 
 class LandingPage extends Component {
   render() {
     return (
-      <div className="landing-page-top">
-        <p>The Vision Vanguards present...</p>
-        <h1>[ SIGHTSTEP ]</h1>
-        <br />
-        <h2>Unleash your inner star: Dance, Sing, and Shine with Karaoke Magic!</h2>
-        <br />
-        <button className="blue-button">
-          GET STARTED &rarr; 
-        </button>
-      </div>
+     <> <div className="landing-page-top">
+          <p>The Vision Vanguards present...</p>
+          <h1>[ SIGHTSTEP ]</h1>
+          <br />
+          <p>Unleash your inner star: Dance, Sing, and Shine with Karaoke Magic!</p>
+          <br />
+          <button className="blue-button" onClick={() => (window.location.href = "/account")}>
+            GET STARTED &rarr;
+          </button>
+        </div>
+        <About/>
+        <Footer/>
+        </>
     );
   }
 }
 
 export default LandingPage;
+
+
