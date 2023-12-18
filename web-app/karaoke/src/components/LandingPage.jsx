46eaffd26864cd7475eb4b75738d7ba3545b9fae
Athulya Ganesh <athulya.ganesh2001@gmail.com>
2023-12-18 14:50:14 +0530

    fonts and landing page

--- web-app/karaoke/src/components/LandingPage.jsx
+++ web-app/karaoke/src/components/LandingPage.jsx
@@ -1,8 +1,12 @@
-function LandingPage() 
-{
-    return (
-        <>This is the landing page. </>
-    )
+import React, { Component } from 'react';
 
+class LandingPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>[ SIGHTSTEP ]</h1>
+      </div>
+    );
+  }
 }
 export default LandingPage; 
