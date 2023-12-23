82f9fe27c5538fdca0379157d3e44b5434e8cc53
mullise147 <mullise@mail.uc.edu>
2023-12-23 13:41:11 -0500

    implement pose estimation

---
+++ CV_Component/PoseModule.py
@@ -0,0 +1,63 @@
+import cv2 as cv
+import mediapipe as mp
+import time
+
+
+class PoseDetector:
+
+    def __init__(self, mode = False, upBody = False, smooth=True, detectionCon = 0.5, trackCon = 0.5):
+
+        self.mode = mode
+        self.upBody = upBody
+        self.smooth = smooth
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
+        self.mpDraw = mp.solutions.drawing_utils
+        self.mpPose = mp.solutions.pose
+        # TODO figure out detectionCon and trackCon parameters for Pose() method
+        self.pose = self.mpPose.Pose(self.mode, self.upBody, self.smooth, self.detectionCon, self.trackCon)
+
+    def findPose(self, img, draw=True):
+        imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+        self.results = self.pose.process(imgRGB)
+        #print(results.pose_landmarks)
+        if self.results.pose_landmarks:
+            if draw:
+                self.mpDraw.draw_landmarks(img, self.results.pose_landmarks, self.mpPose.POSE_CONNECTIONS)
+
+        return img
+
+    def getPosition(self, img, draw=True):
+        lmList= []
+        if self.results.pose_landmarks:
+            for id, lm in enumerate(self.results.pose_landmarks.landmark):
+                h, w, c = img.shape
+                #print(id, lm)
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                lmList.append([id, cx, cy])
+                if draw:
+                    cv.circle(img, (cx, cy), 5, (255, 0, 0), cv.FILLED)
+        return lmList
+
+def main():
+    cap = cv.VideoCapture(0)
+    pTime = 0
+    detector = PoseDetector()
+    while True:
+        success, img = cap.read()
+        img = detector.findPose(img)
+        lmList = detector.getPosition(img)
+        print(lmList)
+
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+
+        cv.putText(img, str(int(fps)), (50, 50), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
+        cv.imshow("Image", img)
+        cv.waitKey(1)
+
+
+if __name__ == "__main__":
+    main()
