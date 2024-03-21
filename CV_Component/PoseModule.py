5b971a228cfe9c0ee820ca46826f032b1c53ea73
mullise147 <mullise@mail.uc.edu>
2024-03-21 13:57:06 -0400

    Hardcoded poses to song

--- CV_Component/PoseModule.py
+++ CV_Component/PoseModule.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 import mediapipe as mp
 import math
+import time
 
 class PoseDetector:
     def __init__(self, mode = False, upBody = False, smooth=True, detectionCon = 0.5, trackCon = 0.5):
@@ -11,27 +12,89 @@ class PoseDetector:
         self.mpDraw = mp.solutions.drawing_utils
         self.mpPose = mp.solutions.pose
         self.pose = self.mpPose.Pose(self.mode, self.upBody, self.smooth)
+        self.score = 0
+        self.rightHOH1 = self.leftHOH1 = self.rightHOH2 = self.leftHOH2 = 0
+        self.yayP = self.HOH = self.rightDivaP = self.leftPush = self.rightBack = 0
+        self.leftRing1 = self.rightRing1 = self.leftRing2 = self.rightRing2 = 0
+        self.leftOh1 = self.rightOh1 = self.leftOh2 = 0
+        self.rightRing3 = self.leftRing3 = self.rightRing4 = self.leftRing4 = 0
 
     def __del__(self):
         self.cap.release()
 
     def getFrame(self):
+        start_time = time.time()
         while True:
             success, frame = self.cap.read()
             if success:
                 frame = self.findPose(frame)
                 self.getPosition(frame)
-                self.rightBreakBack()
-
                 frame = cv.flip(frame, 1)
                 ret, buffer = cv.imencode('.jpg', frame)
                 frame_bytes = buffer.tobytes()
-                
                 yield (b'--frame\r\n'
                     b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
-            else:
-                self.cap.release()
-
+                if start_time <= time.time() < start_time+4 and self.rightHOH1 != 1:
+                    self.rightHOH1 = self.rightHandOnHip()
+                    self.score += self.rightHOH1
+                elif start_time+4 <= time.time() <= start_time+6 and self.leftHOH1 != 1:
+                    self.leftHOH1 = self.leftHandOnHip()
+                    self.score += self.leftHOH1
+                elif start_time+7 <= time.time() <= start_time+8 and self.rightHOH2 != 1:
+                    self.rightHOH2 = self.rightHandOnHip()
+                    self.score += self.rightHOH2
+                elif start_time+9 <= time.time() < start_time+10 and self.leftHOH2 != 1:
+                    self.leftHOH2 = self.leftHandOnHip()
+                    self.score += self.leftHOH2
+                elif start_time+10 <= time.time() < start_time+12 and self.yayP != 1:
+                    self.yayP = self.yay()
+                    self.score += self.yayP
+                elif start_time+12 <= time.time() <= start_time+16 and self.HOH != 1:
+                    self.HOH = self.handsOnHip()
+                    self.score += self.HOH
+                elif start_time+17 <= time.time() <= start_time+21 and self.rightDivaP != 1:
+                    self.rightDivaP = self.rightDiva()
+                    self.score += self.rightDivaP
+                elif start_time+22 <= time.time() <= start_time+26 and self.leftPush != 1:
+                    self.leftPush = self.leftPushAway()
+                    self.score += self.leftPush
+                elif start_time+27 <= time.time() <= start_time+31 and self.rightBack != 1:
+                    self.rightBack = self.rightBreakBack()
+                    self.score += self.rightBack
+                elif start_time+32 <= time.time() <= start_time+33 and self.leftRing1 != 1:
+                    self.leftRing1 = self.leftRingOnIt()
+                    self.score += self.leftRing1
+                elif start_time+34 <= time.time() <= start_time+36 and self.rightRing1 != 1:
+                    self.rightRing1 = self.rightRingOnIt()
+                    self.score += self.rightRing1
+                elif start_time+37 <= time.time() <= start_time+38 and self.leftRing2 != 1:
+                    self.leftRing2 = self.leftRingOnIt()
+                    self.score += self.leftRing2
+                elif start_time+39 <= time.time() <= start_time+41 and self.rightRing2 != 1:
+                    self.rightRing2 = self.rightRingOnIt()
+                    self.score += self.rightRing2
+                elif start_time+42 <= time.time() <= start_time+44 and self.leftOh1 != 1:
+                    self.leftOh1 = self.leftOhOhOh()
+                    self.score += self.leftOh1
+                elif start_time+45 <= time.time() <= start_time+47 and self.rightOh1 != 1:
+                    self.rightOh1 = self.rightOhOhOh()
+                    self.score += self.rightOh1
+                elif start_time+48 <= time.time() <= start_time+51 and self.leftOh2 != 1:
+                    self.leftOh2 = self.leftOhOhOh()
+                    self.score += self.leftOh2
+                elif start_time+52 <= time.time() <= start_time+53 and self.rightRing3 != 1:
+                    self.rightRing3 = self.rightRingOnIt()
+                    self.score += self.rightRing3
+                elif start_time+54 <= time.time() <= start_time+55 and self.leftRing3 != 1:
+                    self.leftRing3 = self.leftRingOnIt()
+                    self.score += self.leftRing3
+                elif start_time+56 <= time.time() <= start_time+58 and self.rightRing4 != 1:
+                    self.rightRing4 = self.rightRingOnIt()
+                    self.score += self.rightRing4
+                elif start_time+59 <= time.time() <= start_time+60 and self.leftRing4 != 1:
+                    self.leftRing4 = self.leftRingOnIt()
+                    self.score += self.leftRing4
+    
     def findPose(self, img, draw=True):
         imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
         self.results = self.pose.process(imgRGB)
@@ -113,8 +176,89 @@ class PoseDetector:
         if (80 <= self.left_arm_bend <= 120 and
             0 <= self.right_leg_bend <= 40):
             print("right break back")
+            return 1
         else:
-            print("...")
+            print("right back")
+
+    def rightDiva(self):
+        if (70 <= self.right_arm_bend <= 110 and
+            75 <= self.left_arm_bend <= 125 and
+            5 <= self.right_leg_bend <= 35):
+            print("right diva pose")
+            return 1
+        else:
+            print("right diva")
+
+    def leftHandOnHip(self):
+        if (75 <= self.left_arm_bend <= 125):
+            print("left hand on hip pose")
+            return 1
+        else:
+            print("left HOH")
+
+    def rightHandOnHip(self):
+        if (75 <= self.right_arm_bend <= 125):
+            print("right hand on hip pose")
+            return 1
+        else:
+            print("right HOH")
+
+    def leftRingOnIt(self):
+        if (100 <= self.left_arm_bend <= 140 and 105 <= self.left_arm_height <= 145 and
+        75 <= self.right_arm_bend <=125):
+            print("left ring on it pose")
+            return 1
+        else:
+            print("left rink")
+
+    def rightRingOnIt(self):
+        if (90 <= self.right_arm_bend <= 150 and 95 <= self.right_arm_height <= 155 and
+        65 <= self.left_arm_bend <=135):
+            print("right ring on it pose")
+            return 1
+        else:
+            print("right ring")  
+
+    def leftPushAway(self):
+        if (50 <= self.left_arm_bend <= 90 and
+        70 <= self.right_arm_height <= 110 and
+        10 <= self.right_leg_bend <= 50):
+            print("left push away pose")
+            return 1
+        else:
+            print("left push")
+        
+    def leftOhOhOh(self):
+        if (120 <= self.right_arm_height <= 170 and 
+        135 <= self.left_leg_bend <= 185):
+            print("Left oh oh oh pose")
+            return 1
+        else:
+            print("left oh")
+
+    def rightOhOhOh(self):
+        if (120 <= self.left_arm_height <= 170 and 
+        135 <= self.right_leg_bend <= 185):
+            print("Right oh oh oh pose")
+            return 1
+        else:
+            print("right oh")
+
+    def yay(self):
+        if (0 <= self.left_arm_bend <= 20 and 10 <= self.left_arm_height <= 50 and
+        0 <= self.right_arm_bend <= 20 and 10 <= self.right_arm_height <= 50):
+            print("Yay pose")
+            return 1
+        else:
+            print("yay")
+
+    def handsOnHip(self):
+        if (75 <= self.left_arm_bend <= 125 and
+        75 <= self.right_arm_bend <=125):
+            print("Hands on hip pose")
+            return 1
+        else:
+            print("HOH")
 
     def leftBreakBack(self):
         if (80 <= self.right_arm_bend <= 120 and
@@ -123,7 +267,7 @@ class PoseDetector:
         else:
             print("...")
                 
-    def leftdiva(self):
+    def leftDiva(self):
         if (70 <= self.left_arm_bend <= 110 and
             75 <= self.right_arm_bend <= 125 and
             5 <= self.left_leg_bend <= 35):
@@ -131,46 +275,13 @@ class PoseDetector:
         else:
             print("...")
 
-    def rightdiva(self):
-        if (70 <= self.right_arm_bend <= 110 and
-            75 <= self.left_arm_bend <= 125 and
-            5 <= self.right_leg_bend <= 35):
-            print("right diva pose")
-        else:
-            print("...")
-
-    def leftHandOnHip(self):
-        if (75 <= self.left_arm_bend <= 125):
-            print("left hand on hip pose")
-        else:
-            print("...")
-
-    def rightHandOnHip(self):
-        if (75 <= self.right_arm_bend <= 125):
-            print("right hand on hip pose")
-        else:
-            print("...")
-
     def bentArmsAboveHead(self):
         if (10 <= self.right_arm_height <= 50 and 100 <= self.right_arm_bend <= 140 and
         10 <= self.left_arm_height <= 50 and 100 <= self.left_arm_bend <= 140):
             print("bent arms above head pose")
+            return 1
         else:
             print("...")
-
-    def leftRingOnIt(self):
-        if (100 <= self.left_arm_bend <= 140 and 105 <= self.left_arm_height <= 145 and
-        75 <= self.right_arm_bend <=125):
-            print("left ring on it pose")
-        else:
-            print("...")
-
-    def rightRingOnIt(self):
-        if (90 <= self.right_arm_bend <= 150 and 95 <= self.right_arm_height <= 155 and
-        65 <= self.left_arm_bend <=135):
-            print("right ring on it pose")
-        else:
-            print("...")  
 
     def rightPushAway(self):
         if (50 <= self.right_arm_bend <= 90 and
@@ -180,46 +291,10 @@ class PoseDetector:
         else:
             print("...")
 
-    def leftPushAway(self):
-        if (50 <= self.left_arm_bend <= 90 and
-        70 <= self.right_arm_height <= 110 and
-        10 <= self.right_leg_bend <= 50):
-            print("left push away pose")
-        else:
-            print("...")
-
     def armsOut(self):
         if (65 <= self.left_arm_height <= 105 and
         65 <= self.right_arm_height <= 105):
             print("arms out pose")
-        else:
-            print("...")
-
-    def leftOhOhOh(self):
-        if (120 <= self.right_arm_height <= 170 and 
-        135 <= self.left_leg_bend <= 185):
-            print("Left oh oh oh pose")
-        else:
-            print("...")
-
-    def rightOhOhOh(self):
-        if (120 <= self.left_arm_height <= 170 and 
-        135 <= self.right_leg_bend <= 185):
-            print("Right oh oh oh pose")
-        else:
-            print("...")
-
-    def yay(self):
-        if (0 <= self.left_arm_bend <= 20 and 10 <= self.left_arm_height <= 50 and
-        0 <= self.right_arm_bend <= 20 and 10 <= self.right_arm_height <= 50):
-            print("Yay pose")
-        else:
-            print("...")
-
-    def handsOnHip(self):
-        if (75 <= self.left_arm_bend <= 125 and
-        75 <= self.right_arm_bend <=125):
-            print("Hands on hip pose")
         else:
             print("...")
 
