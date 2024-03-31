fa2ea7d4e822ee01c7689d36406ac9735a8c37e2
Athulya Ganesh <athulya.ganesh2001@gmail.com>
2024-03-31 18:37:44 -0400

    typing

--- web-app/karaoke/src/components/landing_page/About.jsx
+++ web-app/karaoke/src/components/landing_page/About.jsx
@@ -1,30 +1,3 @@
-// import React, { Component } from 'react';
-// import { Card, Row, Col } from 'antd';
-// import "../../assets/styles/about.css"; 
-// import "../../index.css"; 
-// import Pic from "../../assets/images/profilepic.png"; 
-
-// const { Meta } = Card;
-
-// class About extends Component {
-//   render() {
-//     return (
-//       <div className="landing-page-top">
-//         <h2>ABOUT</h2>
-//         <p className="about-paragraph">
-//           Immerse yourself in the world of music with SightStep, a cutting-edge computer vision application.
-//           Transform any space into your private dance floor and karaoke stage.
-//           SightStep listens to your favorite karaoke songs, tracks your movements, and guides you through dance routines.
-//           Sing along effortlessly as the lyrics sync with your performance.
-//           Elevate your karaoke experience with SightStep's fusion of computer vision, music, and dance.
-//           Get ready to step into a new realm of entertainment!
-//         </p>
-//       </div>
-//     );
-//   }
-// }
-
-// export default About;
 import React, { Component } from 'react';
 import "../../assets/styles/about.css"; 
 
