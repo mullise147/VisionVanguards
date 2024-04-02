805fe7b813e1d00db18423c86afa9d48a7b30d6b
Athulya Ganesh <athulya.ganesh2001@gmail.com>
2024-04-02 13:00:10 -0400

    steph pic + description

--- web-app/karaoke/src/components/performance/Video.jsx
+++
@@ -1,9 +0,0 @@
-const Video = () => {
-    const videoStream = "http://localhost:8080/video-feed"; 
-    return (
-        <div>
-             <img src={videoStream} alt='video-feed' width="390" height="250"/>
-        </div>
-    ); 
-}
-export default Video; 
