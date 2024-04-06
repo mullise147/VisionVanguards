b4a8de7599ac7f2961d12ece4c418c73eeee4ae2
Athulya Ganesh <athulya.ganesh2001@gmail.com>
2024-04-06 19:10:20 -0400

    aurisano picture and description

--- web-app/karaoke/src/components/landing_page/Team.jsx
+++ web-app/karaoke/src/components/landing_page/Team.jsx
@@ -4,6 +4,7 @@ import "../../assets/styles/team.css";
 import Pic from "../../assets/images/profilepic.png";
 import Athulya from "../../assets/images/athulya.png";  
 import Steph from "../../assets/images/steph.png"; 
+import Aurisano from "../../assets/images/aurisano.png"; 
 import "../../assets/styles/font.css"; 
 
 const { Meta } = Card;
@@ -20,7 +21,7 @@ class Team extends Component {
               <Card
                 hoverable
                 style={{ width: 260 }}
-                cover={<img alt="example" src={Athulya} />}
+                cover={<img alt="example" src={Athulya} style={{width: 260, height: 200}}/>}
               >
                 <Meta
                   title={<span  className = "rubik-mono">Athulya</span>}
@@ -33,7 +34,7 @@ class Team extends Component {
               <Card
                 hoverable
                 style={{ width: 260 }}
-                cover={<img alt="example" src={Pic} />}
+                cover={<img alt="example" src={Pic} style={{width: 260, height: 200}}/>}
               >
                 <Meta
                   title={<span  className = "rubik-mono">Rob</span>}
@@ -47,7 +48,7 @@ class Team extends Component {
               <Card
                 hoverable
                 style={{ width: 260 }}
-                cover={<img alt="example" src={Steph} />}
+                cover={<img alt="example" src={Steph} style={{width: 260, height: 200}}/>}
               >
                 <Meta
                   title={<span  className = "rubik-mono">Stephanie</span>}
@@ -60,12 +61,13 @@ class Team extends Component {
               <Card
                 hoverable
                 style={{ width: 260 }}
-                cover={<img alt="example" src={Pic} />}
+                cover={<img alt="example" src={Aurisano} style={{width: 260, height: 200}}/>}
               >
                 <Meta
                   title={<span className = "rubik-mono">Dr. Aurisano</span>}
-                  description={<span className="cousine-regular">In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual
-                  form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</span>}
+                  description={<span className="cousine-regular">Jillian Aurisano, an Assistant Professor at the University of Cincinnati 
+                  and setting up DaVINCI lab, plays a pivotal role as a project advisor. With an academic background that includes a Bachelor's degree in Biology and a PhD from the University of Illinois at 
+                  Chicago, Dr. Aurisano brings a wealth of knowledge and experience to her role.</span>}
                 />
               </Card>
             </Col>
