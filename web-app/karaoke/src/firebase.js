9cd6c95d770862f755d2af7dadb06cb107077eae
athulyaganesh <68687725+athulyaganesh@users.noreply.github.com>
2024-05-10 19:01:46 +0400

    Update firebase.js

--- web-app/karaoke/src/firebase.js
+++ web-app/karaoke/src/firebase.js
@@ -6,17 +6,16 @@ import { Firestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBAi1M4KNRf-EyBh949zRR3CdBuuIx4BdE",
-  authDomain: "karaoke-fd846.firebaseapp.com",
-  projectId: "karaoke-fd846",
-  storageBucket: "karaoke-fd846.appspot.com",
-  messagingSenderId: "91386648390",
-  appId: "1:91386648390:web:8ec442bbb55b49a154f747",
-  measurementId: "G-23JBZCM5YH"
+  apiKey: "yourAPIKey",
+  authDomain: "yourAuthDomain",
+  projectId: "yourProjectID",
+  storageBucket: "yourStorageBucket",
+  messagingSenderId: "yourID",
+  appId: "yourAppID",
+  measurementId: "yourMeasurementID"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
